210548341fa134b834fb99a1ec98b7a28227180c
Amelia Brown <amelia.brown389@gmail.com>
2016-10-31 21:08:39 -0700

    (fix): reinstall dependencies, update package.json

--- server/config/urls.js
+++ server/config/urls.js
@@ -1,6 +1,6 @@
 module.exports = {
   default: 3000,
-  user: process.env.USER || 'http://localhost:3001',
+  user: process.env.ATHENA_USER || 'http://localhost:3001',
   ticket: process.env.TICKET || 'http://localhost:3002',
   kb: process.env.KB || 'http://localhost:3003',
   kbSearch: process.env.KBSEARCH || 'http://localhost:3004',
